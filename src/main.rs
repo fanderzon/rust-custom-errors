f91aa54308dda6820abab0a4c93c6a34697b01b1
Fredrik Andersson <f.anderzon@gmail.com>
2017-02-05 11:08:34 +0100

    Start with initial example of reading and outputting a file

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn main() {
+    // Create a path to the desired file
+    let path = Path::new("files/1.txt");
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        // The `description` method of `io::Error` returns a string that
+        // describes the error
+        Err(why) => panic!("couldn't open {}: {}", path.display(), why.description()),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", path.display(), why.description()),
+        Ok(_) => print!("{} contains:\n{}", path.display(), s),
+    }
+
+    // `file` goes out of scope, and the "hello.txt" file gets closed
+}
